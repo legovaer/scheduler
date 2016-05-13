5d9802f158bd92a57fd24673ba8a8dcecdf0646b
Levi Govaerts <levi.govaerts@capgemini.com>
2016-05-13 13:11:04 +0100

    checkin the config file

--- drupal_ti/before/before_script.sh
+++ drupal_ti/before/before_script.sh
@@ -30,3 +30,4 @@ git apply -v 2189345-39.patch
 
 php /usr/local/simpletest/extensions/coverage/bin/php-coverage-open.php '--include=sites/all/modules/.*\.php$' '--include=sites/all/modules/.*\.inc$' '--include=sites/all/modules/.*\.module$' '--exclude=sites/all/modules/*/tests/.*'
 sudo sed -i "require 'autocoverage.php';" "/home/travis/build/legovaer/drupal-7/drupal/sites/default/settings.php"
+cat /home/travis/build/legovaer/drupal-7/drupal/sites/default/settings.php
