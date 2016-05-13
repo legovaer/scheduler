a096d573cdb2198bcc8b54dbb38c4468e697b279
Levi Govaerts <levi.govaerts@capgemini.com>
2016-05-13 17:10:25 +0100

    Using different ssl protocol

--- drupal_ti/after/generate_badge.php
+++ drupal_ti/after/generate_badge.php
@@ -32,5 +32,5 @@ else {
   $url = "https://img.shields.io/badge/coverage-$percent-$color.svg";
 }
 
-exec("wget https://img.shields.io/badge/coverage-$percent-$color.svg");
+exec("wget https://img.shields.io/badge/coverage-$percent-$color.svg --secure-protocol=tlsv1");
 exec("mv coverage-$percent-$color.svg badge.svg");
