77a2aa4f59b60d337f9eabfd7ad695f4390074a6
Levi Govaerts <levi.govaerts@capgemini.com>
2016-05-13 10:51:09 +0100

    Transformed split line command to one line

--- drupal_ti/before/before_script.sh
+++ drupal_ti/before/before_script.sh
@@ -21,10 +21,5 @@ cd "$DRUPAL_TI_DRUPAL_DIR/$DRUPAL_TI_MODULES_PATH"
 drush en date -y
 
 cd "$DRUPAL_TI_DRUPAL_DIR"
-php /usr/local/simpletest/extensions/coverage/bin/php-coverage-open.php  --
-  '--include=sites/all/modules/.*\.php$'
-  '--include=sites/all/modules/.*\.inc$'
-  '--include=sites/all/modules/.*\.module$'
-  '--exclude=sites/all/modules/*/tests/.*'
-
+php /usr/local/simpletest/extensions/coverage/bin/php-coverage-open.php '--include=sites/all/modules/.*\.php$' '--include=sites/all/modules/.*\.inc$' '--include=sites/all/modules/.*\.module$' '--exclude=sites/all/modules/*/tests/.*'
 sed -i "require 'autocoverage.php';" "$DRUPAL_TI_DRUPAL_DIR/sites/default/settings.php"
