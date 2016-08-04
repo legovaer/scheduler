60e5a2ae7460e0780294050a8ed2d63b4f3bf5c6
Levi Govaerts <legovaer@me.com>
2016-08-04 11:43:18 +0200

    test

---
+++ drupal_ti/before/before_install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Synchronize with drupal.org
+REPO=https://git.drupal.org/project/$DRUPAL_TI_MODULE_NAME.git
+cd $TRAVIS_BUILD_DIR
+ls -ls
+git remote add $REPO drupal
+
+# Check if the branch exists on drupal.org
+exists="git rev-parse --verify --quiet $TRAVIS_BRANCH"
+if [ -n "$exists" ]; then
+  git pull drupal $TRAVIS_BRANCH
+fi
